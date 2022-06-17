575bfcae93438d1d078775fe926b2b83cf8eb31c
Gersi Muca <gm96key@gmail.com>
2022-06-18 01:16:26 +0200

    fixed

--- src/main/java/com/gersimuca/school_project/SchoolProjectApplication.java
+++ src/main/java/com/gersimuca/school_project/SchoolProjectApplication.java
@@ -6,7 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolProjectApplication.class, args);}
-
+	public static void main(String[] args) { SpringApplication.run(SchoolProjectApplication.class, args);}
 }
